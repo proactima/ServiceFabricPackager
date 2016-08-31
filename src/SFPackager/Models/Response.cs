53e71b5609c25269675643bcaa9154eb46114b71
Anders Østhus <anders@proactimasolutions.com>
2016-08-31 23:21:02 +0200

    LocalFileService implemented

--- src/SFPackager/Models/Response.cs
+++ src/SFPackager/Models/Response.cs
@@ -7,6 +7,7 @@ namespace SFPackager.Models
         public BlobOperation Operation { get; set; }
         public HttpStatusCode StatusCode { get; set; }
         public T ResponseContent { get; set; }
+        public string ErrorMessage { get; set; } = string.Empty;
         public bool IsSuccessful => (int)StatusCode < 300; 
     }
 }
--- src/SFPackager/Services/FileStorage/LocalFileService.cs
+++ src/SFPackager/Services/FileStorage/LocalFileService.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
 using System.Threading.Tasks;
 using SFPackager.Interfaces;
 using SFPackager.Models;
@@ -9,17 +12,87 @@ namespace SFPackager.Services.FileStorage
     {
         public Task<Response<string>> GetFileAsStringAsync(string fileName)
         {
-            throw new NotImplementedException();
+            try
+            {
+                if (!File.Exists(fileName))
+                    return Task.FromResult(new Response<string>
+                    {
+                        Operation = BlobOperation.GET,
+                        ErrorMessage = $"File does not exist: {fileName}",
+                        StatusCode = HttpStatusCode.NotFound
+                    });
+
+                var fileContent = File.ReadAllText(fileName);
+                return Task.FromResult(new Response<string>
+                {
+                    Operation = BlobOperation.GET,
+                    ResponseContent = fileContent,
+                    StatusCode = HttpStatusCode.OK
+                });
+            }
+            catch (Exception ex)
+            {
+                return Task.FromResult(new Response<string>
+                {
+                    Operation = BlobOperation.GET,
+                    ErrorMessage = ex.Message,
+                    StatusCode = HttpStatusCode.InternalServerError
+                });
+            }
         }
 
         public Task<Response<byte[]>> GetFileAsBytesAsync(string fileName)
         {
-            throw new NotImplementedException();
+            try
+            {
+                if (!File.Exists(fileName))
+                    return Task.FromResult(new Response<byte[]>
+                    {
+                        Operation = BlobOperation.GET,
+                        ErrorMessage = $"File does not exist: {fileName}",
+                        StatusCode = HttpStatusCode.NotFound
+                    });
+
+                var fileContent = File.ReadAllBytes(fileName);
+                return Task.FromResult(new Response<byte[]>
+                {
+                    Operation = BlobOperation.GET,
+                    ResponseContent = fileContent,
+                    StatusCode = HttpStatusCode.OK
+                });
+            }
+            catch (Exception ex)
+            {
+                return Task.FromResult(new Response<byte[]>
+                {
+                    Operation = BlobOperation.GET,
+                    ErrorMessage = ex.Message,
+                    StatusCode = HttpStatusCode.InternalServerError
+                });
+            }
         }
 
         public Task<Response<string>> SaveFileAsync(string fileName, string content)
         {
-            throw new NotImplementedException();
+            try
+            {
+                File.WriteAllText(fileName, content, Encoding.UTF8);
+                return Task.FromResult(new Response<string>
+                {
+                    Operation = BlobOperation.PUT,
+                    StatusCode = HttpStatusCode.OK,
+                    ResponseContent = string.Empty
+                });
+            }
+            catch (Exception ex)
+            {
+                return Task.FromResult(new Response<string>
+                {
+                    Operation = BlobOperation.PUT,
+                    StatusCode = HttpStatusCode.InternalServerError,
+                    ErrorMessage = ex.Message
+                });
+            }
         }
     }
 }
