240ef99b3c9aea412ca001de0bfcbce0de4723c6
Anders Østhus <anders@proactimasolutions.com>
2016-09-02 01:10:02 +0200

    Moving some manifest stuff out to separate class

---
+++ src/SFPackager/Services/Manifest/ServiceImportMutator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+using SFPackager.Helpers;
+using SFPackager.Models;
+
+namespace SFPackager.Services.Manifest
+{
+    public class ServiceImportMutator : BaseManifestHandler
+    {
+        public void Execute(XmlDocument document, Dictionary<string, GlobalVersion> versions)
+        {
+            var nsManager = GetNsManager(document);
+
+            var serviceNodes = document.GetNodes(
+                "//x:ApplicationManifest/x:ServiceManifestImport/x:ServiceManifestRef", nsManager);
+
+            foreach (var service in serviceNodes)
+            {
+                var serviceElement = service as XmlElement;
+                if (serviceElement == null)
+                    continue;
+
+                var serviceName = serviceElement.GetAttribute("ServiceManifestName");
+                if (!versions.ContainsKey(serviceName))
+                    continue;
+
+                var serviceVersion = versions[serviceName].Version;
+                serviceElement.SetAttribute("ServiceManifestVersion", serviceVersion.ToString());
+            }
+        }
+    }
+}
