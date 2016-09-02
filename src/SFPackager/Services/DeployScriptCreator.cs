a8f15c6f01f89d70335d80ccda04b1af2b525d47
Anders Østhus <anders@proactimasolutions.com>
2016-09-02 17:30:18 +0200

    Working on PS Deploy script

---
+++ src/SFPackager/Services/DeployScriptCreator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SFPackager.Services
+{
+    public class DeployScriptCreator
+    {
+        public void Do()
+        {
+            // Write Generic PS Script
+                // Connect-SFCluster
+                // For each AppType
+                    // Test
+                    // Copy
+                    // Register
+                // For each deployed app
+                    // Start-Upgrade
+        
+            // Replace variables with result from packaging
+        }
+    }
+}
