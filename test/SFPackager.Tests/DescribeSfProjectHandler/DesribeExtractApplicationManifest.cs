eb4c0393b58b9a82458d81517f728f3322e4d8a9
Anders Østhus <anders@proactimasolutions.com>
2016-10-11 19:23:32 +0200

    Improved parsing of SFProj file

---
+++ test/SFPackager.Tests/DescribeSfProjectHandler/DesribeExtractApplicationManifest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+using FluentAssertions;
+using SFPackager.Services;
+using Xunit;
+
+namespace SFPackager.Tests.DescribeSfProjectHandler
+{
+    public class DesribeExtractApplicationManifest
+    {
+        [Fact]
+        public void ItShouldExtractApplicationPackageRootFromSfProj()
+        {
+            string actual;
+            using (var fileStream = new FileStream(@"DescribeSfProjectHandler\Example.sfproj", FileMode.Open))
+            using (var reader = XmlReader.Create(fileStream))
+            {
+                var document = new XmlDocument();
+                document.Load(reader);
+                var manager = new XmlNamespaceManager(document.NameTable);
+                manager.AddNamespace("x", "http://schemas.microsoft.com/developer/msbuild/2003");
+
+                actual = SfProjectHandler.ExtractApplicationManifest(@"c:", document, manager);
+            }
+
+            actual.Should().Be("C:\\ApplicationPackageRoot\\");
+        }
+    }
+}
