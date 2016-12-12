0a41b07c73aacd2ee9889cd80657d5637de6b3f2
Anders Østhus <anders@proactimasolutions.com>
2016-12-12 13:03:27 +0100

    Cleanup and fixes

---
+++ src/SFPackager/Services/Manifest/ApplicationManifestHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using SFPackager.Models;
+using SFPackager.Models.Xml;
+
+namespace SFPackager.Services.Manifest
+{
+    public class ApplicationManifestHandler
+    {
+        public void SetGeneralInfo(
+            ApplicationManifest appManifest,
+            IReadOnlyDictionary<string, GlobalVersion> versions,
+            GlobalVersion appTypeVersion)
+        {
+            appManifest.ApplicationTypeVersion = appTypeVersion.Version.ToString();
+
+            foreach (var serviceImport in appManifest.ServiceManifestImports)
+            {
+                var serviceName = serviceImport.ServiceManifestRef.ServiceManifestName;
+                if (!versions.ContainsKey(serviceName))
+                    continue;
+
+                serviceImport.ServiceManifestRef.ServiceManifestVersion = versions[serviceName].Version.ToString();
+            }
+        }
+    }
+}
---
+++ test/SFPackager.Tests/DescribeManifest/DescribeServiceManifest.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using SFPackager.Models;
+using SFPackager.Models.Xml;
+using SFPackager.Models.Xml.Elements;
+using SFPackager.Services.Manifest;
+using Xunit;
+
+namespace SFPackager.Tests.DescribeManifest
+{
+    public class DescribeServiceManifest
+    {
+        public DescribeServiceManifest()
+        {
+            _serviceManifestHandler = new ServiceManifestHandler();
+        }
+
+        private readonly ServiceManifestHandler _serviceManifestHandler;
+
+        [Fact]
+        public void ItShouldSetVersionOnServiceManifest()
+        {
+            // g
+            var oldVersion = VersionNumber.Create(1, "hash");
+            var newVersion = new GlobalVersion
+            {
+                Version = VersionNumber.Create(2, "newhash")
+            };
+            var serviceManifest = new ServiceManifest
+            {
+                Version = oldVersion.ToString()
+            };
+
+            // w
+            _serviceManifestHandler.SetServiceManifestGeneral(serviceManifest, newVersion);
+
+            // t
+            serviceManifest.Version.Should().Be("2-newhash");
+        }
+
+        [Fact]
+        public void ItShouldSetVersionOnPackages()
+        {
+            // g
+            var currentVersion = VersionNumber.Create(1, "hash");
+            var newVersion = VersionNumber.Create(2, "newhash");
+
+            var serviceManifest = new ServiceManifest
+            {
+                CodePackage = new CodePackage
+                {
+                    Name = "Code",
+                    Version = currentVersion.ToString()
+                },
+                ConfigPackage = new ConfigPackage
+                {
+                    Name = "Config",
+                    Version = currentVersion.ToString()
+                },
+                DataPackages = new List<DataPackage>
+                {
+                    new DataPackage
+                    {
+                        Name = "MyData",
+                        Version = currentVersion.ToString()
+                    }
+                }
+            };
+
+            var parentRef = "Service1";
+
+            var globalVersions = new Dictionary<string, GlobalVersion>
+            {
+                ["thing-Code"] = new GlobalVersion
+                {
+                    ParentRef = parentRef,
+                    PackageType = PackageType.Code,
+                    VersionType = VersionType.ServicePackage,
+                    Version = newVersion
+                },
+                ["thing-Config"] = new GlobalVersion
+                {
+                    ParentRef = parentRef,
+                    PackageType = PackageType.Config,
+                    VersionType = VersionType.ServicePackage,
+                    Version = newVersion
+                },
+                ["thing-MyData"] = new GlobalVersion
+                {
+                    ParentRef = parentRef,
+                    PackageType = PackageType.Data,
+                    VersionType = VersionType.ServicePackage,
+                    Version = newVersion
+                }
+            };
+
+            // w
+            _serviceManifestHandler.SetServicePackagesData(serviceManifest, globalVersions, parentRef);
+
+            // t
+            serviceManifest.CodePackage.Version.Should().Be("2-newhash");
+            serviceManifest.ConfigPackage.Version.Should().Be("2-newhash");
+            serviceManifest.DataPackages.Count.Should().Be(1);
+            serviceManifest.DataPackages.First().Version.Should().Be("2-newhash");
+        }
+    }
+}
